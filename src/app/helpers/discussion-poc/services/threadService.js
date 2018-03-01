39be37babe1819de3ebea0466724917e1f048222
loganathan.shanmugam@tarento.com <loganathan.shanmugam@tarento.com>
2018-03-01 15:44:17 +0530

    Issue #SB-1436 fix: discussion api code refactoring

--- src/app/helpers/discussion-poc/services/threadService.js
+++ src/app/helpers/discussion-poc/services/threadService.js
@@ -16,34 +16,7 @@ class ThreadService {
     /**
      * @property {instance} discussionAdapter - Instance of any discussion engine's adapter like discourse which is used to make a discussion related api calls
      */
-    this.httpService = httpWrapper
-
-    /**
-     * @property {string} userId - An unique id of user
-     */
-    this.userId = userId
-
-    /**
-     * @property {string} discourseEndPoint - An endpoint url for discourse api
-     */
-    this.discourseEndPoint = 'http://172.17.0.2'
-    /**
-     * @property {object} discourseUriList - List of discourse uri's
-     */
-    this.discourseUris = {
-      list: '/tags/',
-      getOne: '/t/',
-      postThread: '/posts',
-      users: '/users',
-      postActions: '/post_actions',
-      acceptAsSolution: '/solution/accept'
-    }
-
-    this.apiAuth = {
-      apiKey: '6d1d27685a7bb8771bc18903d2b980a71b336d7715c28aeb345e3dac65126a47',
-      apiUserName: 'revathipp'
-    }
-
+    this.discussionAdapter = discussionAdapter
   }
 
   /*
