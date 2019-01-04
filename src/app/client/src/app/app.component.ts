0aafecfd690d3e2391457c0b82e7a8d02cfd6a5f
anuj <anujtech46>
2019-01-04 17:08:47 +0530

    botpress config issue

--- src/app/client/src/app/app.component.ts
+++ src/app/client/src/app/app.component.ts
@@ -110,7 +110,7 @@ export class AppComponent implements OnInit {
     this.initTenantService();
     // check botpress
     try {
-      const botpress = (<HTMLInputElement>document.getElementById('exploreButtonVisibility')).value;
+      const botpress = (<HTMLInputElement>document.getElementById('error_handler_plugin')).value;
       if (botpress.toString() === 'true') {
         this.loadBotpress();
       }
